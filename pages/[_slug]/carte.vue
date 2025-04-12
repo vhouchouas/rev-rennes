ff1d74a2b08a4a2a4a8d7f2a5998296694622796
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2025-04-12 20:44:38 +0200

    migrate carte.vue

--- pages/[_slug]/carte.vue
+++ pages/[_slug]/carte.vue
@@ -1,16 +1,19 @@
 <template>
   <ClientOnly>
-    <Map :features="voie.features" :options="mapOptions" class="h-full w-full" />
+    <Map :features="features" :options="mapOptions" class="h-full w-full" />
   </ClientOnly>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import type { Collections } from '@nuxt/content';
+
 const { path } = useRoute();
 const { getVoieCyclableRegex } = useUrl();
 const { getRevName } = useConfig();
 
 const regex = getVoieCyclableRegex();
-const line = path.match(regex)[1];
+const match = path.match(regex);
+const line = match ? match[1] : '';
 
 // https://github.com/nuxt/framework/issues/3587
 definePageMeta({
@@ -27,10 +30,15 @@ const mapOptions = {
   }
 };
 
-const { data: voie } = await useAsyncData(() => {
-  return queryContent('voies-cyclables')
-    .where({ _type: 'json', _path: `/voies-cyclables/ligne-${line}` })
-    .findOne();
+const { data: geojson } = await useAsyncData(() => {
+  return queryCollection('voiesCyclablesGeojson')
+    .path(`/voies-cyclables/ligne-${line}`)
+    .first();
+});
+
+const features: Ref<Collections['voiesCyclablesGeojson']['features']> = computed(() => {
+  if (!geojson.value) return [];
+  return geojson.value.features;
 });
 
 const description = `Carte de la ${getRevName('singular')} ${line}. Découvrez les tronçons prévus, déjà réalisés, en travaux et ceux reportés après 2026.`;
@@ -38,9 +46,9 @@ useHead({
   title: `Carte de la ${getRevName('singular')} ${line}`,
   meta: [
     // description
-    { hid: 'description', name: 'description', content: description },
-    { hid: 'og:description', property: 'og:description', content: description },
-    { hid: 'twitter:description', name: 'twitter:description', content: description }
+    { key: 'description', name: 'description', content: description },
+    { key: 'og:description', property: 'og:description', content: description },
+    { key: 'twitter:description', name: 'twitter:description', content: description }
   ]
 });
 </script>
