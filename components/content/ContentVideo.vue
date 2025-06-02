6687d0089b0f96ca47859647182ea824384fd2d4
bgamard <b.gamard@sismics.com>
2025-06-02 20:04:50 +0200

    add content-video component

---
+++ components/content/ContentVideo.vue
@@ -0,0 +1,27 @@
+<template>
+  <figure class="grid grid-cols-1 justify-items-center">
+    <video
+      controls
+      class="w-full rounded-lg"
+      :src="videoUrl"
+      :alt="caption"
+      width="1310"
+      height="873"
+    />
+    <figcaption v-if="caption" class="text-center">
+      {{ caption }}
+    </figcaption>
+    <div v-if="credit" class="text-base text-center italic">
+      Crédit vidéo : {{ credit }}
+    </div>
+  </figure>
+</template>
+
+<script setup>
+defineProps({
+  videoUrl: { type: String, required: true },
+  link: { type: String, required: false, default: undefined },
+  caption: { type: String, required: false, default: undefined },
+  credit: { type: String, required: false, default: undefined }
+});
+</script>
