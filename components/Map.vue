400319121a8d13161ddd4d3481f97208daf1e58e
r.corre <correronan@gmail.com>
2025-07-10 23:29:52 +0200

    data: add logo without background

--- components/Map.vue
+++ components/Map.vue
@@ -6,7 +6,7 @@
     <img
       v-if="options.logo"
       class="my-0 absolute bottom-0 right-0 z-10"
-      src="https://www.mce-info.org/wp-content/uploads/2024/02/25-Rayons-d-action.webp"
+      src="~/public/images/logo-rayonsdaction.png"
       width="75"
       height="75"
       :alt="`logo ${config.assoName}`"
